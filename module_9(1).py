4e1f1c17a7b9c4c4c18ffce51795baadb3edc43e
band-ald1107 <150985329+band-ald1107@users.noreply.github.com>
2024-11-20 23:48:08 +0800

    Add files via upload

---
+++ module_9(1).py
@@ -0,0 +1,6 @@
+completed_homework = 12
+spent_hours = 1.5
+course_name = 'Python'
+time_per_assignment = spent_hours / completed_homework
+
+print('Курс:', course_name, ', всего задач:', completed_homework, ', затрачено часов:', spent_hours, ', среднее время выполнения', time_per_assignment, 'часа.')
